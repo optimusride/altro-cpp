f4329eb3dd50c36c9be0a66ee4e414eca83380cf
Brian Jackson <bjackson@optimusride.com>
2021-06-09 13:35:21 -0400

    Add the rest of the quadratic cost

---
+++ examples/quadraticcost.hpp
@@ -0,0 +1,104 @@
+#pragma once
+
+#include "eigentypes.hpp"
+#include "costfunction.hpp"
+#include <iostream>
+#include "utils/assert.hpp"
+
+namespace altro {
+namespace examples {
+
+class QuadraticCost : public CostFunction
+{
+ public:
+  QuadraticCost(
+      const MatrixXd& Q, 
+      const MatrixXd& R, 
+      const MatrixXd& H,
+      const VectorXd& q,
+      const VectorXd& r,
+      double c = 0)
+    : n_(q.size()), m_(r.size()), isblockdiag_(H.norm() < 1e-8), Q_(Q), R_(R), H_(H), q_(q), r_(r), c_(c)
+  { 
+    Validate(); 
+  }
+
+  static QuadraticCost LQRCost(const MatrixXd& Q, const MatrixXd& R, 
+                               const VectorXd& xref, const VectorXd& uref)
+  {
+    int n = Q.rows();
+    int m = R.rows();
+    ALTRO_ASSERT(xref.size() == n, "xref is the wrong size.");
+    MatrixXd H = MatrixXd::Zero(n,m);
+    VectorXd q = -2*(Q*xref);
+    VectorXd r = -2*(R*uref);
+    double c = xref.dot(Q * xref) + uref.dot(R * uref);
+    return QuadraticCost(Q,R,H,q,r,c);
+  }
+
+  double Evaluate(const VectorXd& x, const VectorXd& u) const override; 
+  void Gradient(const VectorXd& x, const VectorXd& u, 
+                VectorXd& dx, VectorXd& du) const override;
+  void Hessian(const VectorXd& x, const VectorXd& u, 
+               MatrixXd& dxdx, MatrixXd& dxdu, MatrixXd& dudu) const override; 
+
+  const MatrixXd& GetQ() const { return Q_; }
+  const MatrixXd& GetR() const { return R_; }
+  const MatrixXd& GetH() const { return H_; }
+  const VectorXd& Getq() const { return q_; }
+  const VectorXd& Getr() const { return r_; }
+  double GetConstant() const { return c_; }
+  const Eigen::LDLT<MatrixXd>& GetQfact() const { return Qfact_; }
+  const Eigen::LLT<MatrixXd>& GetRfact() const { return Rfact_; }
+  bool IsBlockDiagonal() const { return isblockdiag_; }
+
+ private:
+  void Validate()
+  {
+    ALTRO_ASSERT(Q_.rows() == n_, "Q has the wrong number of rows");
+    ALTRO_ASSERT(Q_.cols() == n_, "Q has the wrong number of columns");
+    ALTRO_ASSERT(R_.rows() == m_, "R has the wrong number of rows");
+    ALTRO_ASSERT(R_.cols() == m_, "R has the wrong number of columns");
+    ALTRO_ASSERT(H_.rows() == n_, "H has the wrong number of rows");
+    ALTRO_ASSERT(H_.cols() == m_, "H has the wrong number of columns");
+
+    // Check symmetry of Q and R
+    ALTRO_ASSERT(Q_.isApprox(Q_.transpose()), "Q is not symmetric");
+    ALTRO_ASSERT(R_.isApprox(R_.transpose()), "R is not symmetric");
+
+    // Check that R is positive definite
+    Rfact_.compute(R_);
+    ALTRO_ASSERT(Rfact_.info() == Eigen::Success, "R must be positive definite");
+
+    // Check if Q is positive semidefinite
+    Qfact_.compute(Q_);
+    ALTRO_ASSERT(Qfact_.info() == Eigen::Success, 
+      "The LDLT decomposition could of Q could not be computed. Must be positive semi-definite");
+    Eigen::Diagonal<const MatrixXd> D = Qfact_.vectorD();
+    bool ispossemidef = true;
+    for (int i = 0; i < n_; ++i) {
+      if (D(i) < 0) {
+        ispossemidef = false;
+        break;
+      }
+    }
+    ALTRO_ASSERT(ispossemidef, "Q must be positive semi-definite");
+  }
+
+  int n_;
+  int m_;
+  bool isblockdiag_;
+  MatrixXd Q_;   
+  MatrixXd R_;
+  MatrixXd H_;
+  VectorXd q_;
+  VectorXd r_;
+  double c_;
+
+  // decompositions of Q and R
+  Eigen::LDLT<MatrixXd> Qfact_;
+  Eigen::LLT<MatrixXd> Rfact_;
+};
+
+} // namespace examples
+} // namespace altro
---
+++ examples/test/quadraticcost_test.cpp
@@ -0,0 +1,114 @@
+#include <gtest/gtest.h>
+#include "quadraticcost.hpp"
+#include "eigentypes.hpp" 
+#include <iostream>
+
+namespace altro {
+namespace examples {
+
+class QuadraticCostTest : public ::testing::Test 
+{
+ protected:
+  void SetUp() override {
+    Q = Eigen::Vector3d(1,2,3).asDiagonal(); 
+    R = Eigen::Vector2d(4,5).asDiagonal();
+    H = MatrixXd::Zero(3,2);
+    q = Eigen::Vector3d(6,7,8);
+    r = Eigen::Vector2d(9,10);
+    c = 11;
+  }
+  MatrixXd Q; 
+  MatrixXd R; 
+  MatrixXd H; 
+  VectorXd q; 
+  VectorXd r; 
+  double c;
+    
+};
+
+TEST_F(QuadraticCostTest, Construction)
+{
+  QuadraticCost qcost = QuadraticCost(Q, R, H, q, r, c);
+  EXPECT_TRUE(qcost.GetQ().isApprox(Q));
+  EXPECT_TRUE(qcost.GetR().isApprox(R));
+  EXPECT_TRUE(qcost.GetH().isApprox(H));
+  EXPECT_TRUE(qcost.Getq().isApprox(q));
+  EXPECT_TRUE(qcost.Getr().isApprox(r));
+  EXPECT_DOUBLE_EQ(qcost.GetConstant(), c);
+
+  EXPECT_TRUE(qcost.IsBlockDiagonal());
+
+  H(0,0) = 2;
+  QuadraticCost qcost2 = QuadraticCost(Q, R, H, q, r, c);
+  EXPECT_FALSE(qcost2.IsBlockDiagonal());
+}
+
+TEST_F(QuadraticCostTest, ConstructionPSD)
+{
+  // Make Q PSD
+  Q(0,0) = 0;
+  QuadraticCost qcost = QuadraticCost(Q, R, H, q, r, c);
+  EXPECT_TRUE(qcost.GetQ().isApprox(Q));
+  auto Qfact = qcost.GetQfact();
+  EXPECT_DOUBLE_EQ(Qfact.vectorD().minCoeff(), 0);
+}
+
+TEST_F(QuadraticCostTest, ConstructionDeath)
+{
+  // Make R PSD
+  R(0,0) = 0;
+  EXPECT_DEATH(QuadraticCost(Q, R, H, q, r, c), "Assert.*R.*positive definite");
+
+  // Revert back and check again
+  R(0,0) = 1;
+  QuadraticCost qcost = QuadraticCost(Q, R, H, q, r, c);
+
+  // Make Q not semi-definite
+  Q(0,0) = -1;
+  EXPECT_DEATH(QuadraticCost(Q, R, H, q, r, c), "Assert.*Q.*positive semi-definite");
+
+  // Check sizes
+  VectorXd q2 = Eigen::Vector2d(10,12);                     // wrong size
+  VectorXd r2 = Eigen::Vector3d(10,12,13);                  // wrong size
+  MatrixXd Q2 = (MatrixXd(2,2) << 1,2,3,4).finished();      // wrong size
+  MatrixXd Q3 = (MatrixXd(2,3) << 1,2,3,4,5,6).finished();  // not square
+  EXPECT_DEATH(QuadraticCost(Q, R, H, q2, r, c), "Assert.*wrong number of");
+  EXPECT_DEATH(QuadraticCost(Q, R, H, q, r2, c), "Assert.*wrong number of");
+  EXPECT_DEATH(QuadraticCost(Q2, R, H, q, r, c), "Assert.*wrong number of");
+  EXPECT_DEATH(QuadraticCost(Q3, R, H, q, r, c), "Assert.*wrong number of");
+
+  // Check symmetry
+  MatrixXd R2 = (MatrixXd(2,2) << 1,1,0,2).finished();      // asymmetric
+  EXPECT_DEATH(QuadraticCost(Q, R2, H, q, r, c), "Assert.*not symmetric");
+}
+
+TEST_F(QuadraticCostTest, Evaluation)
+{
+  QuadraticCost qcost = QuadraticCost(Q, R, H, q, r, c);
+  VectorXd x = VectorXd::Constant(3, 1.0);
+  VectorXd u = VectorXd::Constant(2, 1.0);
+  double J = Q.sum() + R.sum() + q.sum() + r.sum() + c; 
+  EXPECT_DOUBLE_EQ(J, qcost.Evaluate(x, u));
+
+  x = VectorXd::Random(3);
+  u = VectorXd::Random(2);
+  J = x.dot(Q*x) + u.dot(R*u) + q.dot(x) + r.dot(u) + c;
+  EXPECT_DOUBLE_EQ(J, qcost.Evaluate(x, u));
+
+  VectorXd dx = VectorXd::Zero(3);
+  VectorXd du = VectorXd::Zero(2);
+  qcost.Gradient(x, u, dx, du);
+  EXPECT_TRUE(dx.isApprox(Q*x + q));
+  EXPECT_TRUE(du.isApprox(R*u + r));
+
+  MatrixXd dxdx = MatrixXd::Zero(3,3);
+  MatrixXd dxdu = MatrixXd::Zero(3,2);
+  MatrixXd dudu = MatrixXd::Zero(2,2);
+  qcost.Hessian(x,u, dxdx, dxdu, dudu);
+  EXPECT_TRUE(dxdx.isApprox(Q));
+  EXPECT_TRUE(dxdu.isApprox(H));
+  EXPECT_TRUE(dudu.isApprox(R));
+}
+
+} // namespace examples
+} // namespace altro
